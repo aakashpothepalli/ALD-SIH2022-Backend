ee5bbd1cb461f3644eaf0d578ba69c41574e6461
Aakash Pothepalli <p.saiaakash518@gmail.com>
2022-03-26 10:13:51 +0000

    classify complete

--- api/hospital.js
+++ api/hospital.js
@@ -1,7 +1,9 @@
 
 var express = require('express');
 var router = express.Router();
-const { Deta } = require("deta")
+const { Deta } = require("deta");
+const { default: axios } = require('axios');
+const { response } = require('express');
 const deta = Deta("c0zjinx1_bmnWXrhDYAmgwJf2vHzuDE8CvRjYyhAB")
 const hospitalDB = deta.Base("hospital")
 
@@ -58,9 +60,54 @@ router.post('/classify',async function (req,res){
       }
     }
     // API Call to retrieve Category from ML Model
-
+    console.log("here")
     let category = null
+    let response = null
+    try{
+       response = await axios.post('https://api-inference.huggingface.co/models/Supreeth/BioBERT',{
+        "inputs":req.body['diagnosis']
+        },{
+          headers: {
+            "Authorization": "Bearer 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"
+          }
+        })
 
+        // LABEL 1 -> red
+        // LABEL 2 -> Yellow
+        // LABEL 0 -> Green
+      // res.status(200).send(response.data)
+    }
+    catch(e){
+      console.log(e)
+      res.status(400).send("Error in ML Model")
+      return ;
+    }
+    let red = 0 , yellow = 0 , green = 0;
+    let data = response.data[0]
+    console.log(data)
+    for(let i = 0 ; i < data.length ; i++){
+        if(data[i]['label']=='LABEL_0'){
+            green=data[i]['score']
+        }
+        else if(data[i]['label']=='LABEL_1'){
+            red = data[i]['score']
+        }
+        else if(data[i]['label']=='LABEL_2'){
+            yellow = data[i]['score']
+        }
+    } 
+    console.log(red,yellow,green)
+    if(red>yellow && red>green){
+        category = 'red'
+    }
+    else if(yellow>red && yellow>green){
+        category = 'yellow'
+    }
+    else if(green>red && green>yellow){
+        category = 'green'
+    }
+
+    // res.status(200).send({red,yellow,green,category})
     const diagnosisDB = deta.Base("diagnosis")
     diagnosisDB.put({
         did:req.body['did'],
@@ -68,7 +115,7 @@ router.post('/classify',async function (req,res){
         hid:req.body['hid'],
         category:category
     })
-    res.status(200).send({category,message:"Successfully classified"});
+    res.status(200).send({category,message:"Successfully classified",red,yellow,green});
 })
 
 module.exports = router;
